a12605c881d0c84a1ebd0bb82dedb7157e0d7984
TaosifShaad <taosifshaad000@gmail.com>
2025-02-11 17:38:29 -0600

    add complete todo feature

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 import './Todo.css'
 
 export const Todo = ({tasks}) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckbox = (e) => {
+    setIsChecked(e.target.checked);
+    tasks.completed = !tasks.completed;
+  }
+
   return (
     <div className='todo'>
-        <p>{tasks.task}</p>
+        <input type='checkbox' checked={isChecked} onChange={handleCheckbox} />
+        <p className={tasks.completed? 'completed': ''}>{tasks.task}</p>
         <div>
         <FontAwesomeIcon icon={faPenToSquare} />
         <FontAwesomeIcon icon={faTrash} />
